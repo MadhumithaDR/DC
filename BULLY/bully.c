a46e8b8312de3ea7be24d3a0953a1022aa9bb306
Madhumitha D R <105427629+MadhumithaDR@users.noreply.github.com>
2024-04-07 13:06:09 +0530

    Update bully.c

--- BULLY/bully.c
+++ BULLY/bully.c
@@ -1,128 +1,85 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#define MAX 10
-int list[MAX],n,c;
-void display()
-{
-int i;
-printf("\nProcesses-->");
-for(i=1;i<=n;i++)
-printf("\t %d",i);
-printf("\nAlive-->");
-for(i=1;i<=n;i++)
-printf("\t %d",list[i]);
-printf("\ncoordinator is::%d",c);
+#include <stdio.h>
+#include <stdbool.h>
+
+#define NUM_PROCESSES 7
+
+struct Process {
+    int id;
+    bool coordinator;
+    bool active;
+};
+
+struct Process processes[NUM_PROCESSES];
+int coordinator_id = -1; // Initially no coordinator
+
+void process_failure(int process_id) {
+    printf("Process %d has failed.\n", process_id);
+    processes[process_id].active = false;
+    if (process_id == coordinator_id) {
+        coordinator_id = -1; // Reset coordinator ID
+    }
 }
-void bully()
-{
-int ch,crash,activate,i,gid,flag,subcdr;
-do
-{
-printf("\n1.Crash\n2.Activate\n3.Display\n4.Exit\nEnter You choice::");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1:
-printf("\nEnter Process no. to Crash::");
-scanf("%d",&crash);
-if(list[crash])
-list[crash]=0;
-else
-{
-printf("\nProcess is alreaady dead!!");
-break;
-}
-do
-{
-printf("\nEnter election generator id::");
-scanf("%d",&gid);
-if(gid==c)
-{
-printf("\nenter a valid generator id::");
-}
-}while(gid==crash);
-flag=0;
-if(crash==c)
-{
-for(i=gid+1;i<=n;i++)
-{
-printf("\nmessage is sent from %d to %d",gid,i);
-if(list[i])
-{
-subcdr=i;
-printf("Response is sent from %d to %d",i,gid);
-flag=1;
-}
-}
-if(flag==1)
-{
-c=subcdr;
-}
-else
-{
-c=gid;
-}
-}
-display();
-break;
-case 2:
-printf("\nEnter Process no. to Activated::");
-scanf("%d",&activate);
-if(!list[activate])
-list[activate]=1;
-else
-{
-printf("\nProcess is alreaady alive!!");
-break;
-}
-if(activate==n)
-{
-c=n;
-break;
-}
-for(i=activate+1;i<=n;i++)
-{
-printf("\nmessage is sent from %d to %d",activate,i);
-if(list[i])
-{
-subcdr=i;
-printf("Response is sent from %d to %d",i,activate);
-flag=1;
-}
-}
-if(flag==1)
-{
-c=subcdr;
-}
-else
-{
-c=activate;
-}
-display();
-break;
-case 3:
-display();
-break;
-case 4:
-break;
-}
-}while(ch!=4);
-}
-int main()
-{
-int i,j;
-printf("\nEnter no. of process::");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-printf("\nEnter Process %d is Alive or not(0/1)::",i);
-scanf("%d",&list[i]);
-if(list[i])
-c=i;
-}
-display();
-printf("\nBULLY ALGORITHM\n");
-bully();
-return 0;
+
+int main() {
+    // Initialize processes
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        processes[i].id = i;
+        processes[i].coordinator = false;
+        processes[i].active = true;
+    }
+
+    // Simulate failure of process with ID 3
+    int failed_process_id;
+    printf("Enter the process_id of failed process:");
+    scanf ("%d",&failed_process_id);
+    process_failure(failed_process_id);
+
+    // Assume process initiates the election and sends a message to the next higher-priority process
+    int initiating_process_id; // Change as required
+    printf("Enter the process which initiates election: ");
+    scanf("%d",&initiating_process_id);
+    printf("Process %d initiates election.\n", initiating_process_id);
+
+    for (int i = initiating_process_id + 1; i < NUM_PROCESSES; i++) {
+        if (processes[i].active) {
+            printf("Process %d sends election message to Process %d\n", initiating_process_id, i);
+            // Assume there is a function to send messages
+
+            // Simulate process forwarding the election message to the next higher-priority process
+            printf("Process %d forwards election message to Process %d\n", i, i+1);
+            // Assume there is a function to send messages
+
+            break;
+        }
+    }
+    printf("Process %d forwards election message to Process %d\n",NUM_PROCESSES-2,NUM_PROCESSES-1);
+
+    // Assume the last process receives the election message and accepts to become the coordinator
+    int last_process_id;
+    for(int i=NUM_PROCESSES-1;i>=initiating_process_id;i--)
+    {
+        if(processes[i].active)
+        {
+            last_process_id=i;
+            break;
+        }
+
+    }
+
+    printf("Process %d accepts to become coordinator.\n", last_process_id);
+
+    // After message passing is completed, the last process becomes the coordinator
+    printf("Process %d becomes coordinator.\n", last_process_id);
+    processes[last_process_id].coordinator = true;
+    coordinator_id = last_process_id;
+
+    // Inform other processes about becoming coordinator
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        if (i != last_process_id && processes[i].active) {
+            printf("Process %d sends coordinator announcement to Process %d\n", last_process_id, i);
+            // Assume there is a function to send messages
+        }
+    }
+
+    return 0;
 }
